54a17914772e7dd944cd16c8a4dcbcadbf29973e
Lukas Valatka <lukas.valatka123@gmail.com>
2015-02-15 18:08:44 +0200

    Add @base_title in testing to avoid repetition

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Buzzy - the Social Network"
+  end
+
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Buzzy - the Social Network"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Buzzy - the Social Network"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | Buzzy - the Social Network"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
