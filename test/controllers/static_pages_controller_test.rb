e3328112960f36f33cd2eedbd90c0f4a161d965d
Lukas Valatka <lukas.valatka123@gmail.com>
2015-02-15 17:59:26 +0200

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Buzzy - the Social Network"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Buzzy - the Social Network"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+    assert_select "title", "About | Buzzy - the Social Network"
   end
 
 end
